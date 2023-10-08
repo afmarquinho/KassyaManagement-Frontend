c473a48c2fd5486ff3f26a4fa600c84e607a9a31
afmarquinho <fernandez9029@gmail.com>
2023-10-07 19:47:33 -0500

    ok modal de editar

--- src/kassyaApp/sections/pages/supplier/SupplierViewPage.jsx
+++ src/kassyaApp/sections/pages/supplier/SupplierViewPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import SupplierLayout from "../../../layout/SupplierLayout";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getOneSuppilerAsync } from "../../../../redux/thunks/supplierThunks";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
@@ -8,13 +8,19 @@ import { Container, Row, Col } from "react-bootstrap";
 import { ModalEditSupplier } from "../../../components";
 
 const SupplierViewPage = () => {
+  const supplier = useSelector((state) => state.supplier.oneSupplier);
+  const [activeModal, setActiveModal] = useState(false);
+
   const params = useParams();
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getOneSuppilerAsync(params.id));
   }, []);
-  const supplier = useSelector((state) => state.supplier.oneSupplier);
+
+  const abrirModal = () => {
+    setActiveModal(true);
+  };
 
   return (
     <SupplierLayout>
@@ -22,7 +28,31 @@ const SupplierViewPage = () => {
         <Container>
           <Row>
             <Col xs={12} className="mb-5 d-flex">
-              <ModalEditSupplier /> 
+              {activeModal && (
+                <ModalEditSupplier
+                  supplier={supplier}
+                  activeModal={activeModal}
+                  setActiveModal={setActiveModal}
+                />
+              )}
+
+              <Button className="btn btn-primary me-5" onClick={abrirModal}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+                Editar
+              </Button>
               <Button className="btn btn-danger">
                 {" "}
                 <svg
