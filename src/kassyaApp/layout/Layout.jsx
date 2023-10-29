44d2e3991f29afa511992bb9fa9cfaf27c1ea6c8
afmarquinho <fernandez9029@gmail.com>
2023-10-28 21:24:45 -0500

    estilizacion del menu supplier

--- src/kassyaApp/layout/Layout.jsx
+++ src/kassyaApp/layout/Layout.jsx
@@ -3,18 +3,20 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="bg-customDeepBlue flex items-center w-full justify-between h-10">
-        <div className="flex items-center gap-3">
-          <h1>Kassya Management</h1>
-          <span className="text-slate-300 text-sm hidden md:block">
-            Signed in as:{" "}
-            <span className="text-customMainColor">Mark Otto</span>
-          </span>
+      <div className="mask h-10">
+        <div className="bg-customDeepBlue flex items-center justify-between h-10 fixed top-0 w-full z-10">
+          <div className="flex items-center gap-3">
+            <h1>Kassya Management</h1>
+            <span className="text-slate-300 text-sm hidden md:block">
+              Signed in as:{" "}
+              <span className="text-customMainColor">Mark Otto</span>
+            </span>
+          </div>
+          <Navbar />
         </div>
-        <Navbar />
       </div>
-      {/* 
-      <div>{children}</div> */}
+
+      <div>{children}</div>
     </>
   );
 };
